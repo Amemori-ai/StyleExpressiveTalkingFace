277d3ed6df93c34dc33e590afa2694ecffb3eecf
wanghaoran <wanghaoran@szglint.com>
2024-07-24 14:45:26 +0800

    [update new features]
    *: aligner.py:
       update offsetNet class module, which supports fourier-like feature
    
       add offsetNetV2 class module, supports vgg encoder.
    
    *: module.py:
       add "BaseConv2d"

--- TalkingFace/module.py
+++ TalkingFace/module.py
@@ -5,13 +5,10 @@ class BaseLinear(nn.Module):
                  input_channels,
                  output_channels,
                  skip: bool = False,
-                 batchnorm: bool = False
+                 norm: str = None
                  ):
         super().__init__()
-        if batchnorm:
-            self.module = nn.Sequential(*[nn.Linear(input_channels,output_channels), nn.BatchNorm1d(output_channels)])
-        else:
-            self.module = nn.Sequential(*[nn.Linear(input_channels,output_channels)])
+        self.module = nn.Sequential(*[nn.Linear(input_channels,output_channels), getattr(nn, norm)(output_channels)])
         self.act = nn.LeakyReLU()
         self.skip = skip
         if skip:
@@ -26,3 +23,52 @@ class BaseLinear(nn.Module):
             return self.act(res + self.skip_module(x))
         else:
             return self.act(res)
+
+class BaseConv2d(nn.Module):
+    def __init__(
+                 self,
+                 input_channels,
+                 output_channels,
+                 k,
+                 s,
+                 p,
+                 norm = "BatchNorm2d",
+                 act = "ReLU",
+                 repeat = 0
+                ):
+        super().__init__()
+
+        if act == "ReLU":
+            args = dict()
+        else:
+            args = dict(negative_slope = 0.1)
+
+        modules = [
+                    nn.Conv2d(input_channels, output_channels, k, s, p),
+                    getattr(nn, norm)(output_channels),
+                    getattr(nn, act)(**args)
+                  ]
+
+
+        if repeat > 0:
+            modules += [
+                        nn.Conv2d(output_channels, output_channels, k, s, p),
+                        getattr(nn, norm)(output_channels),
+                        getattr(nn, act)(**args)
+                       ]
+
+        self.base_conv = nn.Sequential(*modules)
+
+    def forward(self, x):
+        return self.base_conv(x)
+
+class Flatten(nn.Module):
+    def forward(
+                self,
+                x
+               ):
+        n = x.shape[0]
+        return x.reshape(n, -1)
+
+
+
